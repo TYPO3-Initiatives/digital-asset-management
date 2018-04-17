4675cbff653cd02b15c6e75afa51f251150f4f36
Jürgen Venne <venne@schaffrath-digital.de>
2018-04-17 09:34:47 +0200

    [BUGFIX] add module use correct name

--- ext_tables.php
+++ ext_tables.php
@@ -1,35 +1,35 @@
 <?php
-//defined('TYPO3_MODE') or die();
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-//    'file',
-//    'list',
-//    'top',
-//    '',
-//    [
-//        'routeTarget' => \TYPO3\CMS\DigitalAssetManagement\Controller\DigitalAssetManagementController::class . '::handleRequest',
-//        'access' => 'user,group',
-//        'name' => 'Digital Asset Management',
-//        'icon' => 'EXT:digital_asset_management/Resources/Public/Icons/module-dam.svg',
-//        'labels' => 'LLL:EXT:digital_asset_management/Resources/Private/Language/locallang_module_dam.xlf'
-//    ]
-//);
-
-
-
-
-
-// Register "Styleguide" backend module
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'TYPO3.CMS.digital_asset_management',
+defined('TYPO3_MODE') or die();
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
     'file',
-    'list',
+    'DigitalAssetManagement',
     'top',
+    '',
     [
-        'DigitalAssetManagement' => 'index'
-    ],
-    [
+        'routeTarget' => \TYPO3\CMS\DigitalAssetManagement\Controller\DigitalAssetManagementController::class . '::index',
         'access' => 'user,group',
+        'name' => 'file_DigitalAssetManagement',
         'icon' => 'EXT:digital_asset_management/Resources/Public/Icons/module-dam.svg',
         'labels' => 'LLL:EXT:digital_asset_management/Resources/Private/Language/locallang_module_dam.xlf'
     ]
 );
+
+
+
+//
+//
+//// Register "Styleguide" backend module
+//\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+//    'TYPO3.CMS.digital_asset_management',
+//    'file',
+//    'list',
+//    'top',
+//    [
+//        'DigitalAssetManagement' => 'index'
+//    ],
+//    [
+//        'access' => 'user,group',
+//        'icon' => 'EXT:digital_asset_management/Resources/Public/Icons/module-dam.svg',
+//        'labels' => 'LLL:EXT:digital_asset_management/Resources/Private/Language/locallang_module_dam.xlf'
+//    ]
+//);
