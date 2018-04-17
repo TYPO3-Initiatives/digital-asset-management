3e177794b0234736afc7addcf7046e3a92d99cb8
Jürgen Venne <venne@schaffrath-digital.de>
2018-04-17 11:14:46 +0200

    [TASK] introduce overview template

--- Classes/Controller/DigitalAssetManagementController.php
+++ Classes/Controller/DigitalAssetManagementController.php
@@ -1,15 +1,108 @@
 <?php
-
-
+declare(strict_types = 1);
 namespace TYPO3\CMS\DigitalAssetManagement\Controller;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\HtmlResponse;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3Fluid\Fluid\View\ViewInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+
+/**
+ * Backend controller: The "Digital Asset Management" module
+ *
+ * Optional replacement of filelist
+ */
 class DigitalAssetManagementController
 {
-    public function handleRequest(){
+    /**
+     * @var ModuleTemplate
+     */
+    protected $moduleTemplate;
 
+    /**
+     * @var ViewInterface
+     */
+    protected $view;
+
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+//        $this->siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+        $this->moduleTemplate = GeneralUtility::makeInstance(ModuleTemplate::class);
     }
 
-    public function index(){
-        return '<h1>Test</h1>';
+    /**
+     * Main entry method: Dispatch to other actions - those method names that end with "Action".
+     *
+     * @param ServerRequestInterface $request the current request
+     * @return ResponseInterface the response with the content
+     */
+    public function handleRequest(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->moduleTemplate->getPageRenderer()->loadRequireJsModule('TYPO3/CMS/Backend/ContextMenu');
+        $action = $request->getQueryParams()['action'] ?? $request->getParsedBody()['action'] ?? 'overview';
+        $this->initializeView($action);
+//            \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($action);
+        $result = call_user_func_array([$this, $action . 'Action'], [$request]);
+        if ($result instanceof ResponseInterface) {
+            return $result;
+        }
+        $this->moduleTemplate->setContent($this->view->render());
+        return new HtmlResponse($this->moduleTemplate->renderContent());
     }
+
+    /**
+     * @param string $templateName
+     */
+    protected function initializeView(string $templateName): void
+    {
+        $this->view = GeneralUtility::makeInstance(StandaloneView::class);
+        $this->view->setTemplate($templateName);
+        $this->view->setTemplateRootPaths(['EXT:digital_asset_management/Resources/Private/Templates']);
+        $this->view->setPartialRootPaths(['EXT:digital_asset_management/Resources/Private/Partials']);
+        $this->view->setLayoutRootPaths(['EXT:digital_asset_management/Resources/Private/Layouts']);
+    }
+
+    /**
+     * List pages that have 'is_siteroot' flag set - those that have the globe icon in page tree.
+     * Link to Add / Edit / Delete for each.
+     */
+    protected function overviewAction(): void
+    {
+//        $this->configureOverViewDocHeader();
+        /** @var Site[] $allSites */
+//        $allSites = $this->siteFinder->getAllSites();
+//        $pages = $this->getAllSitePages();
+//        foreach ($allSites as $identifier => $site) {
+//            $rootPageId = $site->getRootPageId();
+//            if (isset($pages[$rootPageId])) {
+//                $pages[$rootPageId]['siteIdentifier'] = $identifier;
+//                $pages[$rootPageId]['siteConfiguration'] = $site;
+//            }
+//        }
+        $this->view->assign('filemounts', 'nope');
+    }
+
+
+
+
 }
