db98ffaae8ee1fe513b20249120e5f4d52e80887
Jürgen Venne <venne@schaffrath-digital.de>
2018-04-19 09:10:57 +0200

    [WIP] get items by ajax route

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -4,8 +4,8 @@
 use TYPO3\CMS\DigitalAssetManagement\Controller\DigitalAssetManagementAjaxController;
 return [
     //
-    'dam_inline_show_folder' => [
-        'path' => '/dam/inline/showfolder',
-        'target' => DigitalAssetManagementAjaxController::class . '::showFolder'
+    'dam_request' => [
+        'path' => '/dam/request',
+        'target' => DigitalAssetManagementAjaxController::class . '::handleAjaxRequestAction'
     ]
 ];
--- Resources/Public/JavaScript/DigitalAssetManagementActions.js
+++ Resources/Public/JavaScript/DigitalAssetManagementActions.js
@@ -13,9 +13,55 @@
 
 "use strict";
 
-var todo = {},
-    todo.dam = {};
+require(['jquery'], function($) {
+    $(function() {
+        todo.dam.init();
+    });
+});
+
+var todo = {};
+todo.dam = {};
 
 todo.dam.init = function (){
-
+    $.getJSON(TYPO3.settings.ajaxUrls.dam_request, {'getContent': '/'})
+    .done(function(data){
+        todo.dam.renderContent(data);
+    })
+    .fail(function(err){
+        console.log('todo.dam. request promise fail ' + JSON.stringify(err));
+        top.TYPO3.Notification.warning('Request failed', 'Der Inhalt can not be displayed. ' + err.readyState);
+        todo.dam.renderError(err);
+    });
 };
+
+todo.dam.renderError = function(err){
+    $('.errorlog').html(err.responseText);
+};
+
+todo.dam.renderContent = function(data){
+    if (data && data.request){
+        $('.errorlog').html(data.request + data.response);
+    }
+    if (data.content && data.content.files || data.content.folder){
+        //Show files and folders
+        var html ="";
+        for (var i=0; i < data.content.files.length; i++){
+            var file = data.content.files[i];
+            console.log(file);
+            //@todo: use moment.js for date-formatting?!
+            html += '<div class="card d-inline-block" style="width: 180px;">\n' +
+                '\t\t\t\t\t\t\t\t\t<img class="card-img-top" src="PlaceholderImgae" data-src="'+file.uid+'" width="180c" height="120c"/>\n' +
+                '\t\t\t\t\t\t\t\t\t<div class="card-body">\n' +
+                '\t\t\t\t\t\t\t\t\t\t<h5 class="card-title">'+file.name+'</h5>\n' +
+                '\t\t\t\t\t\t\t\t\t\t<p class="card-text">Size: '+file.size+' <br>Modified: '+file.uid+' '+''+'</p>\n' +
+                '\t\t\t\t\t\t\t\t\t\t<a href="#" class="btn btn-primary">Go somewhere</a>\n' +
+                '\t\t\t\t\t\t\t\t\t</div>\n' +
+                '\t\t\t\t\t\t\t\t</div>\n';
+        }
+        $('.files').html(html);
+    } else {
+        //Show storage infos
+    }
+};
+
+
