bb825c8bfa3d00a3ef233c3a20d9a7124d9ad3a8
Jürgen Venne <venne@schaffrath-digital.de>
2018-04-20 11:05:33 +0200

    [TASK] use indexAction, remove unused code

--- Classes/Controller/DigitalAssetManagementController.php
+++ Classes/Controller/DigitalAssetManagementController.php
@@ -15,15 +15,13 @@ namespace TYPO3\CMS\DigitalAssetManagement\Controller;
  * The TYPO3 project - inspiring people to share!
  */
 
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Core\Http\HtmlResponse;
-use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3Fluid\Fluid\View\ViewInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-
 
 /**
  * Backend controller: The "Digital Asset Management" module
@@ -52,41 +50,6 @@ class DigitalAssetManagementController
     }
 
     /**
-     * Main entry method: Dispatch to other actions - those method names that end with "Action".
-     *
-     * @param ServerRequestInterface $request the current request
-     * @return ResponseInterface the response with the content
-     */
-    public function handleRequest(ServerRequestInterface $request): ResponseInterface
-    {
-//        $this->moduleTemplate->getPageRenderer()->loadRequireJsModule('TYPO3/CMS/Backend/ContextMenu');
-        //include JavaScript and CSS
-        $this->moduleTemplate->getPageRenderer()->loadRequireJsModule('TYPO3/CMS/DigitalAssetManagement/DigitalAssetManagementActions');
-//        $this->moduleTemplate->loadJavascriptLib('EXT:digital_asset_management/Resources/Public/JavaScript/DigitalAssetManagementActions.js');
-//        $this->moduleTemplate->loadJavascriptLib('https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js');
-        $this->moduleTemplate->getPageRenderer()->addCssFile('EXT:digital_asset_management/Resources/Public/Css/digitalassetmanagement.css');
-        //Include bootstrap css
-        //@todo: include bootstrap css from TYPO3 not from CDN, how to?
-        $this->moduleTemplate->getPageRenderer()->addCssFile('https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css');
-        $action = $request->getQueryParams()['action'] ?? $request->getParsedBody()['action'] ?? 'overview';
-        $this->initializeView($action);
-//            \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($action);
-        $result = call_user_func_array([$this, $action . 'Action'], [$request]);
-        if ($result instanceof ResponseInterface) {
-            return $result;
-        }
-
-        // Add shortcut button
-        $buttonBar = $this->moduleTemplate->getDocHeaderComponent()->getButtonBar();
-        $myButton = $buttonBar->makeShortcutButton()->setModuleName('my_info');
-        $buttonBar->addButton($myButton);
-
-
-        $this->moduleTemplate->setContent($this->view->render());
-        return new HtmlResponse($this->moduleTemplate->renderContent());
-    }
-
-    /**
      * @param string $templateName
      */
     protected function initializeView(string $templateName): void
@@ -99,71 +62,28 @@ class DigitalAssetManagementController
     }
 
     /**
-     * List pages that have 'is_siteroot' flag set - those that have the globe icon in page tree.
-     * Link to Add / Edit / Delete for each.
-     */
-    protected function overviewAction(): void
-    {
-        $backendUser = $this->getBackendUser();
-
-        // Get all storage objects
-        /** @var ResourceStorage[] $fileStorages */
-        $fileStorages = $backendUser->getFileStorages();
-        $content = $this->getFolderContent($fileStorages);
-         $this->view->assign('storages', $fileStorages);
-        $this->view->assign('content', $content);
-        $this->view->assign('user', $backendUser);
-    }
-
-    /**
-     * Return first level of files/folders/storages as an assoziative array
-     * this can be:
-     *  if there is only one storage the content of that storage is returned
-     *  if there are more than one storages the storages are returned
+     * Main entry method: Dispatch to other actions - those method names that end with "Action".
      *
-     * @param ResourceStorage[] $fileStorages
-     * @return array
-     * @throws \RuntimeException
+     * @param ServerRequestInterface $request the current request
+     * @return ResponseInterface the response with the content
      */
-    protected function getFolderContent($fileStorages){
-        //If there is only one storage show content of that as entrypoint
-        if (is_array($fileStorages)){
-            if (count($fileStorages) === 1) {
-                /** @var ResourceStorage $fileStorage  */
-                $fileStorage = reset($fileStorages);
-                if ($fileStorage) {
-                    $rootfolder = $this->folderObject = $fileStorage->getRootLevelFolder();
-                    $fileIdentifiers = $fileStorage->getFileIdentifiersInFolder($rootfolder->getIdentifier());
-                    $folderIdentifiers = $fileStorage->getFolderIdentifiersInFolder($rootfolder->getIdentifier());
-                    $files = $fileStorage->getFilesInFolder($rootfolder);
-                    $folders = $fileStorage->getFoldersInFolder($rootfolder);
-                } else {
-                    throw new \RuntimeException('Could not find any folder to be displayed.', 1349276894);
-                }
-            } else {
-
-            }
-        }
-        return ['files' => $files, 'folders' => $folders];
-    }
-
-    /**
-     * Returns an instance of LanguageService
-     *
-     * @return \TYPO3\CMS\Core\Localization\LanguageService
-     */
-    protected function getLanguageService()
+    public function handleRequest(ServerRequestInterface $request): ResponseInterface
     {
-        return $GLOBALS['LANG'];
-    }
-
-    /**
-     * Returns the current BE user.
-     *
-     * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
-     */
-    protected function getBackendUser()
-    {
-        return $GLOBALS['BE_USER'];
+        //        $this->moduleTemplate->getPageRenderer()->loadRequireJsModule('TYPO3/CMS/Backend/ContextMenu');
+        //include JavaScript and CSS
+        $this->moduleTemplate->getPageRenderer()->loadRequireJsModule('TYPO3/CMS/DigitalAssetManagement/DigitalAssetManagementActions');
+        //        $this->moduleTemplate->loadJavascriptLib('EXT:digital_asset_management/Resources/Public/JavaScript/DigitalAssetManagementActions.js');
+        //        $this->moduleTemplate->loadJavascriptLib('https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js');
+        $this->moduleTemplate->getPageRenderer()->addCssFile('EXT:digital_asset_management/Resources/Public/Css/digitalassetmanagement.css');
+        //Include bootstrap css
+        //@todo: include bootstrap css from TYPO3 not from CDN, how to?
+        $this->moduleTemplate->getPageRenderer()->addCssFile('https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css');
+        $this->initializeView('index');
+        // Add shortcut button
+        $buttonBar = $this->moduleTemplate->getDocHeaderComponent()->getButtonBar();
+        $myButton = $buttonBar->makeShortcutButton()->setModuleName('my_info');
+        $buttonBar->addButton($myButton);
+        $this->moduleTemplate->setContent($this->view->render());
+        return new HtmlResponse($this->moduleTemplate->renderContent());
     }
 }
