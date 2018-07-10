9bc07121b24013ff71b352b6712a3500a6aa4f17
jokumer <service@enobe.de>
2018-07-10 20:20:44 +0200

    Raise dependency to TYPO3 9.x
    This package use classes, wich are only available in newer TYPO3 versions fx. HtmlResponse

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.9.99',
+            'typo3' => '9.0.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
