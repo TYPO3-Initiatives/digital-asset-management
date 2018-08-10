124827f08c88def2391cab2fb022014f2d6d2567
Jürgen Venne <venne@schaffrath-digital.de>
2018-08-10 17:55:03 +0200

    [TASK] code style and use IIFE in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -7,20 +7,23 @@
  * LICENSE file that was distributed with this source code.
  */
 
-defined('TYPO3_MODE') or die();
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-    'file',
-    'DigitalAssetManagement',
-    'top',
-    '',
-    [
-        'routeTarget' => \TYPO3\CMS\DigitalAssetManagement\Controller\DigitalAssetManagementController::class . '::handleRequest',
-        'access' => 'user,group',
-        'name' => 'file_DigitalAssetManagement',
-        'icon' => 'EXT:digital_asset_management/Resources/Public/Icons/module-dam.svg',
-        'labels' => 'LLL:EXT:digital_asset_management/Resources/Private/Language/locallang_mod.xlf',
-        'workspaces' => 'online,custom',
-        'navigationComponentId' => '',
-        'inheritNavigationComponentFromMainModule' => false
-    ]
-);
+defined('TYPO3_MODE') || die();
+
+(function() {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+        'file',
+        'DigitalAssetManagement',
+        'top',
+        '',
+        [
+            'routeTarget' => \TYPO3\CMS\DigitalAssetManagement\Controller\DigitalAssetManagementController::class . '::handleRequest',
+            'access' => 'user,group',
+            'name' => 'file_DigitalAssetManagement',
+            'icon' => 'EXT:digital_asset_management/Resources/Public/Icons/module-dam.svg',
+            'labels' => 'LLL:EXT:digital_asset_management/Resources/Private/Language/locallang_mod.xlf',
+            'workspaces' => 'online,custom',
+            'navigationComponentId' => '',
+            'inheritNavigationComponentFromMainModule' => false
+        ]
+    );
+})();
