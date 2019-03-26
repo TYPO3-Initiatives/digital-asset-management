343f3b019c6e34cf103b633070c634727678f487
Andreas Fernandez <andreas.fernandez@typo3.com>
2019-03-26 15:54:26 +0100

    [WIP][TASK] Initial commit for very basic, POC folder tree

--- Build/Vue/src/components/Tree/index.tsx
+++ Build/Vue/src/components/Tree/index.tsx
@@ -8,9 +8,19 @@ export default class Tree extends Vue {
     constructor(props: any) {
         super(props);
     }
+
     private render(h: CreateElement): VNode {
         return (
-            <div>Tree</div>
+            <div id='typo3-pagetree' class='svg-tree'>
+                <div>
+                    <div id='typo3-pagetree-treeContainer'>
+                        <div id='typo3-pagetree-tree' class='svg-tree-wrapper' style='height:1000px;'>
+                            <div class='node-loader'></div>
+                        </div>
+                    </div>
+                </div>
+                <div class='svg-tree-loader'></div>
+            </div>
         );
     }
 }
