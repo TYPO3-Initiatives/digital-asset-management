6a2fed71458bac09215e3771192ab82a350036df
Susanne Moog <susanne.moog@typo3.com>
2019-03-27 11:17:30 +0100

    [TASK] Allow all origins for use in TYPO3

--- Build/Vue/vue.config.js
+++ Build/Vue/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   configureWebpack: {
+    devServer: {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+    },
     externals: [
       {
         // Object
