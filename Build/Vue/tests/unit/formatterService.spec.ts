affef22f6043bd11b58b730b6282dbb627bb1371
Susanne Moog <susanne.moog@typo3.com>
2019-04-04 23:10:27 +0200

    [TASK] Add formatterService tests

--- Build/Vue/tests/unit/formatterService.spec.ts
+++ Build/Vue/tests/unit/formatterService.spec.ts
@@ -2,8 +2,20 @@ import { expect } from 'chai';
 import FormatterService from '@/services/FormatterService';
 
 describe('FormatterService', () => {
+  it('formats kilo bytes', () => {
+    const testSize = 1000;
+    expect(FormatterService.fileSizeAsString(testSize)).to.equal('1 KB');
+  });
   it('formats mega bytes', () => {
-    const testSize = 1024;
-    expect(FormatterService.fileSizeAsString(1024)).to.equal('1 MB');
+    const testSize = 1048577;
+    expect(FormatterService.fileSizeAsString(testSize)).to.equal('1 MB');
+  });
+  it('rounds to kilobytes', () => {
+    const testSize = 10485;
+    expect(FormatterService.fileSizeAsString(testSize)).to.equal('10 KB');
+  });
+  it('rounds to megabytes', () => {
+    const testSize = 2156789;
+    expect(FormatterService.fileSizeAsString(testSize)).to.equal('2 MB');
   });
 });
