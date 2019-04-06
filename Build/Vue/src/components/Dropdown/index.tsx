aebd6bf5f78f5b3d7a7153414d80a954dfe89126
benjaminkott <benjamin.kott@outlook.com>
2019-04-06 22:01:41 +0200

    [TASK] Introduce dropdown component

---
+++ Build/Vue/src/components/Dropdown/index.tsx
@@ -0,0 +1,72 @@
+import {VNode} from 'vue';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import Icon from '@/components/Icon';
+
+@Component
+export default class Dropdown extends Vue {
+    @Prop()
+    toggleLabel!: string;
+
+    @Prop()
+    toggleLabelIconIdentifier!: string;
+
+    @Prop()
+    toggleTitle!: string;
+
+    @Prop()
+    toggleAriaLabel!: string;
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    private static toggleDropdown(e: Event): void {
+        const me = (e.target as HTMLElement);
+        const dropdown = me.closest('.component-dropdown');
+        if (dropdown === null) {
+            return;
+        }
+
+        const dropdownToggle = dropdown.querySelector('.component-dropdown-toggle');
+        if (dropdownToggle !== null) {
+            let isActive = dropdownToggle.getAttribute('aria-expanded') === 'true' ? true : false;
+            dropdownToggle.setAttribute('aria-expanded', (!isActive).toString());
+        }
+    }
+
+    private render(): VNode {
+        const toggleLabelIcon = this.toggleLabelIconIdentifier ? (
+            <span class='component-dropdown-toggle-icon' role='presentation'>
+                <Icon identifier={this.toggleLabelIconIdentifier} />
+            </span>
+        ) : '';
+        const toggleLabel = this.toggleLabel ? (
+            <span class='component-dropdown-toggle-text'>
+                {this.toggleLabel}
+            </span>
+        ) : '';
+
+        return (
+            <span class='component-dropdown'>
+                <button
+                    type='button'
+                    class='component-dropdown-toggle'
+                    title={this.toggleTitle}
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                    aria-label={this.toggleAriaLabel}
+                    onclick={Dropdown.toggleDropdown}
+                >
+                    {toggleLabelIcon}
+                    {toggleLabel}
+                    <span class='component-dropdown-toggle-icon' role='presentation'>
+                        <Icon identifier='apps-pagetree-expand' />
+                    </span>
+                </button>
+                <div class='component-dropdown-container'>
+                    {this.$slots.default}
+                </div>
+            </span>
+        );
+    }
+}
--- Build/Vue/src/components/StorageSelector/index.tsx
+++ Build/Vue/src/components/StorageSelector/index.tsx
@@ -1,6 +1,8 @@
-import Icon from '@/components/Icon';
+import Dropdown from '@/components/Dropdown';
+import DropdownMenu from '@/components/DropdownMenu';
 import {Mutations} from '@/enums/Mutations';
 import {StorageInterface} from '@/interfaces/StorageInterface';
+import {LinkInterface} from '@/interfaces/LinkInterface';
 import {Component, Vue} from 'vue-property-decorator';
 import {VNode} from 'vue';
 import {Action, State} from 'vuex-class';
@@ -20,23 +22,6 @@ export default class StorageSelector extends Vue {
         super(props);
     }
 
-    private static toggleDropdown(e: Event): void {
-        const me = (e.target as HTMLElement);
-        const dropdown = me.closest('.component-dropdown');
-        if (dropdown === null) {
-            return;
-        }
-
-        let isActive = dropdown.classList.contains('component-dropdown-active');
-        dropdown.classList.toggle('component-dropdown-active', !isActive);
-        dropdown.classList.toggle('component-dropdown-inactive', isActive);
-
-        const dropdownToggle = dropdown.querySelector('.component-dropdown-toggle');
-        if (dropdownToggle !== null) {
-            dropdownToggle.setAttribute('aria-expanded', (!isActive).toString());
-        }
-    }
-
     private render(): VNode | null {
         if (!this.activeStorage) {
             return null;
@@ -46,54 +31,29 @@ export default class StorageSelector extends Vue {
             return null;
         }
 
-        const options = this.storages.map(this.generateOption, this);
+        const entries = this.storages.map(this.generateEntry, this);
         return (
-            <span class='component-storageselector component-dropdown component-dropdown-inactive'>
-                <button
-                    type='button'
-                    title={TYPO3.lang['StorageSelector.title']}
-                    aria-label={TYPO3.lang['StorageSelector.label']}
-                    class='component-dropdown-toggle'
-                    aria-haspopup='true'
-                    aria-expanded='false'
-                    onclick={StorageSelector.toggleDropdown}
+            <span class='component-storageselector'>
+                <Dropdown
+                    toggleLabel={this.activeStorage.name}
+                    toggleLabelIconIdentifier={this.activeStorage.icon}
+                    toggleTitle={TYPO3.lang['StorageSelector.title']}
+                    toggleAriaLabel={TYPO3.lang['StorageSelector.label']}
                 >
-                    <span class='component-dropdown-toggle-icon' role='presentation'>
-                        <Icon identifier={this.activeStorage.icon} />
-                    </span>
-                    <span class='component-dropdown-toggle-text'>
-                        {this.activeStorage.name}
-                    </span>
-                    <span class='component-dropdown-toggle-icon' role='presentation'>
-                        <Icon identifier='apps-pagetree-expand' />
-                    </span>
-                </button>
-                <div class='component-dropdown-container'>
-                    <ul class='component-dropdown-menu' role='menu'>
-                        {options}
-                    </ul>
-                </div>
+                    <DropdownMenu entries={entries} />
+                </Dropdown>
             </span>
         );
     }
 
-    private generateOption(storage: StorageInterface): VNode {
-        return(
-            <li class='component-dropdown-menu-item'>
-                <a
-                    class='component-dropdown-menu-link'
-                    title={storage.storageName}
-                    href='#'
-                    data-identifier={storage.identifier}
-                    onclick={this.updateStorage}
-                >
-                    <span class='component-dropdown-menu-link-icon' role='presentation'>
-                        <Icon identifier={storage.icon} />
-                    </span>
-                    <span class='component-dropdown-menu-link-text'>{storage.storageName}</span>
-                </a>
-            </li>
-        );
+    private generateEntry(storage: StorageInterface): LinkInterface {
+        return {
+            title: storage.storageName,
+            href: '#',
+            dataIdentifier: storage.identifier.toString(),
+            iconIdentifier: storage.icon,
+            onclick: this.updateStorage,
+        };
     }
 
     private updateStorage(e: Event): void {
