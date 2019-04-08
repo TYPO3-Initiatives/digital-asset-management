84d9d68e7343e92915c9ab5a737220a4f23b6a53
benjaminkott <benjamin.kott@outlook.com>
2019-04-08 10:22:37 +0200

    [TASK] Use official namespace typo3 and set community as author

--- Classes/Entity/FolderItemFolder.php
+++ Classes/Entity/FolderItemFolder.php
@@ -2,7 +2,7 @@
 declare(strict_types = 1);
 
 /*
- * This file is part of the package lns/digital-asset-management.
+ * This file is part of the package typo3/cms-digital-asset-management.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
