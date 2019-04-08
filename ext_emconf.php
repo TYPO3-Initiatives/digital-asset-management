b5bfdbc2872f0f6b979064d334ffe89b8209e633
Benjamin Kott <benjamin.kott@outlook.com>
2019-04-08 10:42:32 +0200

    [TASK] Use official typo3 namespace and set community as author (#114)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the package lns/digital-asset-management.
+ * This file is part of the package typo3/cms-digital-asset-management.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Through this module you can upload, copy, move an delete files on the configured file storages of your TYPO3 instance.',
     'category' => 'file',
     'state' => 'experimental',
-    'author' => 'Schaffrath DigitalMedien',
-    'author_email' => 'info@schaffrath-digital.de',
+    'author' => 'TYPO3 Community',
+    'author_email' => '',
     'author_company' => '',
     'version' => '0.0.1',
     'autoload' => [
